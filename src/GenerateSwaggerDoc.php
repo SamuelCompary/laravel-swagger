20ebd51daff73d142fb73e1f454c00e8336be2ef
Samuel Ahlström <82799765+SamuelCompary@users.noreply.github.com>
2021-04-27 11:23:16 +0200

    Update GenerateSwaggerDoc.php

--- src/GenerateSwaggerDoc.php
+++ src/GenerateSwaggerDoc.php
@@ -3,6 +3,7 @@
 namespace Mtrajano\LaravelSwagger;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
 
 class GenerateSwaggerDoc extends Command
 {
@@ -14,7 +15,8 @@ class GenerateSwaggerDoc extends Command
     protected $signature = 'laravel-swagger:generate
                             {--format=json : The format of the output, current options are json and yaml}
                             {--f|filter= : Filter to a specific route prefix, such as /api or /v2/api}
-                            {--o|output= : Output file to write the contents to, defaults to stdout}';
+                            {--o|output= : Output file to write the contents to, defaults to stdout}
+                            {--u|upload= : Upload file to provider}';
 
     /**
      * The console command description.
@@ -33,15 +35,27 @@ class GenerateSwaggerDoc extends Command
         $config = config('laravel-swagger');
         $filter = $this->option('filter') ?: null;
         $file = $this->option('output') ?: null;
+        $upload = $this->option('upload') ?: null;
 
         $docs = (new Generator($config, $filter))->generate();
 
         $formattedDocs = (new FormatterManager($docs))
             ->setFormat($this->option('format'))
             ->format();
+        $this->info(gettype($formattedDocs));
 
         if ($file) {
             file_put_contents($file, $formattedDocs);
+        } elseif ($upload) {
+            $tmpFile = tmpfile();
+            $stream = stream_get_meta_data($tmpFile)['uri'];
+            $writer = fopen($stream, 'w');
+            fputs($writer, $formattedDocs);
+
+            Storage::disk($upload)->putFileAs(
+                config('constants.uploads.folder_export'), $stream, 'swagger-' . \App::environment() . '.json'
+            );
+            $this->line('Uploaded file to azure');
         } else {
             $this->line($formattedDocs);
         }
